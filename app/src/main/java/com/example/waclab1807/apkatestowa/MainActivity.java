0f60040a4948cc3dc6b5b67b135b9d4316ab5bfd
SkylinR <skylinr7@gmail.com>
2016-02-24 21:31:13 +0100

    initial commit

--- app/src/main/java/com/example/waclab1807/apkatestowa/MainActivity.java
+++ app/src/main/java/com/example/waclab1807/apkatestowa/MainActivity.java
@@ -15,7 +15,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    //COMMENT
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
